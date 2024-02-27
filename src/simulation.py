64a0985e6d8da71e7a9bc0a75b56f1fd056bf42a
nathan <nathan.e.mcguire@gmail.com>
2024-02-27 12:47:49 -0500

    Simple 'random' exploration behavior, basic ant world and visualizer

---
+++ src/simulation.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+"""
+Class to run the simulation
+
+Contains a world and a list of ant colony classes, manages the updates of each
+"""
+
+import typing
+
+from world import AntWorld
+from ant import Ant
+
+
+class Simulation(object):
+    def __init__(self, world: AntWorld):
+        self.world = world
+
+        self.ants: typing.List[Ant] = []
+
+    def addAnt(self, ant: Ant, x: float, y: float):
+        if self.world.isFreePosition(x, y):
+            self.ants.append(ant)
+            ant.setWorld(self.world)
+            ant.setPosition(x, y)
+        else:
+            pass
+            # TODO: This really shouldn't happen, but we should probably do something here
+
+    def getWorld(self):
+        return self.world
+
+    def getAnts(self):
+        return self.ants
+
+    def runOnce(self, delta_t=0.1):
+        """
+        Function to advance the simulation one time step
+        :param delta_t: time step length, in seconds
+        :return:
+        """
+
+        # Update the world
+        self.world.runOnce(delta_t)
+
+        # Update the ants
+        for ant in self.ants:
+            ant.runOnce(delta_t)
